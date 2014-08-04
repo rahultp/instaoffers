d0c1720de37c7fd37201ebd5533d6c814d945eac
root <root@instaoffers.co>
2014-08-04 04:30:11 -0400

    Add Dashboard Login

--- views/login.js
+++ views/login.js
@@ -3,22 +3,29 @@
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     console.log(response);
+FB.api('/me', function(response) {
+    console.log(JSON.stringify(response));
+});
+FB.api('/me/permissions', function(response) {
+    console.log(JSON.stringify(response));
+});
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
+	$('#loginModal').modal('hide');
+	window.location.href = "http://instaoffers.co/dashboard";
       // Logged into your app and Facebook.
-      testAPI();
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
+      //  document.getElementById('status').innerHTML = 'Please log ' +
+      //  'into this app.';
+      FB.login();
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
+      FB.login();
     }
   }
 
@@ -67,14 +74,4 @@
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-  }
 </script>
